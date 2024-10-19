c7be13d76c9e1b5b269cbb2db9e53aa8e8d9fa83
Aanchal-shrma <aanchalsharma13604@gmail.com>
2024-10-19 14:20:01 -0400

    add files

---
+++ index.js
@@ -0,0 +1,10 @@
+let locationData = document.querySelector(".loc-data");
+
+const Location = [
+    city = "ghaziabad India"
+
+];
+
+let loc = Location.map((data) => {
+    locationData.innerHTML = data;
+})
